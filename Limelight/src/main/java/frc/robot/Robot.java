c957d2e8ffd8b8ae452bea668f4b6a2f245d8551
NotMaxwell <william.max.mccormick@gmail.com>
2025-11-07 15:07:54 -0600

    converted to loggedrobot instead of timed

--- Limelight/src/main/java/frc/robot/Robot.java
+++ Limelight/src/main/java/frc/robot/Robot.java
@@ -1,19 +1,31 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+// Copyright...
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
 
-  private final RobotContainer m_robotContainer;
+  public Robot() {}
 
-  public Robot() {
+  @Override
+  public void robotInit() {
+    Logger.recordMetadata("ProjectName", "YourRobot");
+    Logger.recordMetadata("BuildDate", "2025");
+
+    // Log to USB if present, otherwise NT
+    Logger.addDataReceiver(new WPILOGWriter("/media/sda1/")); 
+    Logger.addDataReceiver(new NT4Publisher());
+
+    Logger.start();
+
     m_robotContainer = new RobotContainer();
   }
 
@@ -26,12 +38,6 @@ public class Robot extends TimedRobot {
   public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
@@ -41,32 +47,15 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousPeriodic() {}
-
-  @Override
   public void autonomousExit() {}
 
   @Override
   public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    if (m_autonomousCommand != null) m_autonomousCommand.cancel();
   }
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void teleopExit() {}
 
   @Override
   public void testInit() {
     CommandScheduler.getInstance().cancelAll();
   }
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void testExit() {}
 }
